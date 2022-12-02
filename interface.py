8c2c46dfbded3f663be721cd38146b7188d50fe5
Kulakov Anton <kulakov_3000@mail.ru>
2022-12-02 17:31:09 +0300

    импорт

--- interface.py
+++ interface.py
@@ -2,19 +2,19 @@ import delete
 import write
 import print1
 import edit
-import add
 import search
+import import_file
 
 while True:
     print("База данных сотрудников.\n")
-    print('1. Вывести все записи.\n2. Добавить запись.\n3. Найти запись.\n4. Изменить запись.\n5. Удалить запись.\n6. Выход.\n')
+    print('1. Вывести все записи.\n2. Добавить запись.\n3. Найти запись.\n4. Изменить запись.\n5. Удалить запись.\n6. Импорт данных.\n7. Выход.\n')
 
     value = int(input("Выберите действие: "))
     print(value)
     if value == 1:
-        print1.print_all_to_console()
+        print1.print_all_to_console('employees.csv')
     elif value == 2:
-        add.reading
+        write.New_Entry()
     elif value == 3:
         search.Search_Entry('employees.csv')
     elif value == 4:
@@ -22,6 +22,8 @@ while True:
     elif value == 5:
         delete.delete_str('employees.csv')
     elif value == 6:
+        import_file.import_file(input("Введите зазвание файла: "), 'employees.csv')
+    elif value == 7:
         break
 
 
--- write.py
+++ write.py
@@ -4,7 +4,6 @@ def New_Entry():
     name = input('Введите имя: ')
     father_name = input('Введите отчество: ')
     phone = input('Введите номер телефона: ')
-    department = input('Введите отдел: ')
-    position = input('Введите должность: ')
+
     with open('employees.csv','a', encoding='utf-8') as book:
-        book.write(f'{ID}, {sirname}, {name}, {father_name}, {phone}, {department}, {position};\n')
+        book.write(f'{ID}, {sirname}, {name}, {father_name}, {phone};\n')
